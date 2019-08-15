1e2b8816ed981e1efaa38d3bcff72f2f8be170b9
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2019-08-15 11:36:51 +0200

    Add modal to space for adding an address

--- src/containers/Spaces/Space.tsx
+++ src/containers/Spaces/Space.tsx
@@ -1,47 +1,71 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { Sidebar, Detail } from '../../components/Space';
+import { ModalAddItem } from '../../components/Space/Modals';
 
 type Props = {
   // no-op
 }
 
 type State = {
-  // no-op
+  isModalAddItemOpen: boolean
 }
 
 export class Space extends React.Component<Props, State> {
+  state: Readonly<State> = {
+    isModalAddItemOpen: false
+  };
+
+  toggleModalAddItem() {
+    this.setState({
+      isModalAddItemOpen: !this.state.isModalAddItemOpen
+    });
+  }
+
+  onAddItem(base58check: string) {
+    console.log("Adding address", base58check);
+  }
+
   render(): JSX.Element {
+    const { isModalAddItemOpen } = this.state;
+
     return (
-      <Container fluid className="p-0 d-flex flex-column flex-grow-1">
-        <Row noGutters className="flex-grow-1 bg-light">
-          <Col className="bg-white shadow" lg="2">
-            <Sidebar searchPlaceholder="Search Cluster" btnText="Add Item" onBtnClick={() => console.log('btnblick')} />
-          </Col>
+      <>
+        <Container fluid className="p-0 d-flex flex-column flex-grow-1">
+          <Row noGutters className="flex-grow-1 bg-light">
+            <Col className="bg-white shadow" lg="2">
+              <Sidebar searchPlaceholder="Search Cluster" btnText="Add Item" onBtnClick={() => this.toggleModalAddItem()} />
+            </Col>
 
-          <Col style={{overflow: 'hidden'}}>
-            {/*<div className="h-100" ref={this.graphElementDiv}>
-              <ApolloConsumer>
-                {client => (
-                  <Graph graphData={graph} 
-                    height={mainElementHeight} width={mainElementWidth} 
-                    onSelectItem={(item: any) => this.loadAddress(item, client)}
-                    onExpandIncomingTransactions={(base58check: string) => this.onExpandIncomingTransactions(client, base58check)}
-                    onExpandOutgoingTransactions={(base58check: string) => this.onExpandOutgoingTransactions(client, base58check)}
-                    onDelete={(base58check: string) => this.onDelete(base58check)}
-                  />
-                )}
-              </ApolloConsumer>
-            </div>*/}
-          </Col>
+            <Col style={{overflow: 'hidden'}}>
+              {/*<div className="h-100" ref={this.graphElementDiv}>
+                <ApolloConsumer>
+                  {client => (
+                    <Graph graphData={graph} 
+                      height={mainElementHeight} width={mainElementWidth} 
+                      onSelectItem={(item: any) => this.loadAddress(item, client)}
+                      onExpandIncomingTransactions={(base58check: string) => this.onExpandIncomingTransactions(client, base58check)}
+                      onExpandOutgoingTransactions={(base58check: string) => this.onExpandOutgoingTransactions(client, base58check)}
+                      onDelete={(base58check: string) => this.onDelete(base58check)}
+                    />
+                  )}
+                </ApolloConsumer>
+              </div>*/}
+            </Col>
 
-          {this.renderDetail(null)}
-        </Row>
+            {this.renderDetail(null)}
+          </Row>
 
-        {/*<ApolloConsumer>
-          {client => (<ModalAddItem isOpen={false} toggle={() => console.log('toggle')} onAddItem={(base58check: string) => console.log(base58check)} />)}
-        </ApolloConsumer>*/}
-      </Container>
+          {/*<ApolloConsumer>
+            {client => (<ModalAddItem isOpen={false} toggle={() => console.log('toggle')} onAddItem={(base58check: string) => console.log(base58check)} />)}
+          </ApolloConsumer>*/}
+        </Container>
+
+        <ModalAddItem 
+            isOpen={isModalAddItemOpen} 
+            toggle={() => this.toggleModalAddItem()} 
+            onAddItem={this.onAddItem} />
+      </>
     );
   }
 
