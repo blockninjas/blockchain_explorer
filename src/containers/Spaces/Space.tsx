0a05d6db9d1e8a0195a83b5a32f81b1d8701d7da
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2019-08-25 07:51:01 +0200

    Extend GraphQL query to load block creation times in detail HOC to further calculate first and last used timestamps

--- src/containers/Spaces/Space.tsx
+++ src/containers/Spaces/Space.tsx
@@ -79,11 +79,40 @@ const GET_ADDRESS = gql`
       base58check
       incoming {
         value
+        transaction {
+          block {
+            creationTime
+          }
+          hash
+          inputs {
+            address {
+              base58check
+            }
+          }
+          outputs {
+            address {
+              base58check
+            }
+          }
+        }
       }
       outgoing {
         value
         transaction {
+          block {
+            creationTime
+          }
           hash
+          inputs {
+            address {
+              base58check
+            }
+          }
+          outputs {
+            address {
+              base58check
+            }
+          }
         }
       }
       tags {
@@ -182,16 +211,16 @@ const Space: FunctionComponent<RouteComponentProps<RouteProps>> = ({ match }) =>
 }
 
 const NodeDetail: FunctionComponent<{base58check: string}> = (props) => {
-  const { loading, error, data }= useQuery(GET_ADDRESS, { variables: { base58check: props.base58check }});
+  const { loading, error, data }= useQuery<{address: TypedQueryAddress}>(GET_ADDRESS, { variables: { base58check: props.base58check }});
   if (loading) return <div>Loading...</div>;
   if (error) { 
     console.error(error);
     return <Alert color="danger">Sorry, could not fetch address for given crypto hash.</Alert>;
   }
 
-  const { address } = data;
+  const address = data!.address;
   const base58check = address.base58check;
-  const priorityTag =  address.tags.length >= 1 && address.tags.reduce((tagL: any, tagR: any) => tagL.priority < tagR.priority ? tagR: tagL).title;
+  const priorityTag =  address.tags.length >= 1 ? address.tags.reduce((tagL: any, tagR: any) => tagL.priority < tagR.priority ? tagR: tagL).title : "";
   const balanceReceived = address.incoming.reduce((sum: number, current: any) => sum + current.value, 0)
   const balanceSent = address.outgoing.reduce((sum: number, current: any) => sum + current.value, 0)
   const balanceBtc = balanceReceived - balanceSent;
@@ -238,6 +267,7 @@ type TypedQueryInOut = {
 
 type TypedQueryTransaction = {
   hash: string;
+  block: TypedQueryBlock;
   inputs: Array<{
     address: {
       base58check: string;
@@ -248,6 +278,10 @@ type TypedQueryTransaction = {
       base58check: string;
     }
   }>
+}
+
+type TypedQueryBlock = {
+  creationTime: string;
 }
 
 type TypedQueryTags = {
